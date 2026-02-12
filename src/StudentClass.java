e9b5d7e7d2695f64885e2cda9648bcf32fdf9fe1
hariyudeghub <harikrishnas9a@gmail.com>
2026-02-12 09:30:00 +0530

    Added

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -10,24 +10,24 @@ class Student {
     }
    
     void display(){
-        System.out.println("Name: " + name + " Roll No: " + rollNo);
+        System.out.println("Name: " + name + " Roll: " + rollNo);
     }
 }
 
 public class StudentClass {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-	String name1=sc.nextLine();
-	String roll1=sc.nextLine();
+	    String name1=sc.nextLine();
+	    String roll1=sc.nextLine();
 
-	String name2=sc.nextLine();
-	String roll2=sc.nextLine();
+	    String name2=sc.nextLine();
+	    String roll2=sc.nextLine();
 
-	Student s1=new Student(name1,roll1);
-	Student s2=new Student(name2,roll2);
+	    Student s1=new Student(name1,roll1);
+	    Student s2=new Student(name2,roll2);
 	
         s1.display();
-	s2.display();
+	    s2.display();
 
  	}
 }
